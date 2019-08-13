0f4c887e721ba23099f39580a7b6a13482d5cb76
Owain Roberts <owain.roberts@bjss.com>
2019-08-13 09:08:45 +0100

    EE-6825 Added a "Test" that currently does not do anything so that we can check that the container is starting up and running its tests

---
+++ src/test/java/uk/gov/digital/ho/pttg/SmokeTest.java
@@ -0,0 +1,13 @@
+package uk.gov.digital.ho.pttg;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+public class SmokeTest {
+
+    @Test
+    public void testStartup() {
+        // TODO EE-6825 Tests yet to be added - for now it is sufficient to check that the tests run
+    }
+}
