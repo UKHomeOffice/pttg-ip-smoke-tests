560fd76a0b6c1ae0c757543c743ee725d5631098
Owain Roberts <owain.roberts@bjss.com>
2019-08-15 10:55:27 +0100

    EE-6825 Intentionally making tests fail to ensure that Drone is going to fail in this case

--- src/test/java/uk/gov/digital/ho/pttg/SmokeTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/SmokeTest.java
@@ -2,10 +2,13 @@ package uk.gov.digital.ho.pttg;
 
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public class SmokeTest {
 
     @Test
     public void testStartup() {
+        fail("Intentionally failing to ensure failure is noticed by Drone");
         // TODO EE-6825 Tests yet to be added - for now it is sufficient to check that the tests run
     }
 }
