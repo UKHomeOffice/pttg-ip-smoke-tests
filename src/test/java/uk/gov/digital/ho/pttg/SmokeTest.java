882afe54c8180f47ea543cdd5b17c69467b4f4b4
Owain Roberts <owain.roberts@bjss.com>
2019-08-16 12:29:17 +0100

    EE-6825 Addressing another earlier PR comment - replacing GSON with FasterXML JSON

--- src/test/java/uk/gov/digital/ho/pttg/SmokeTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/SmokeTest.java
@@ -1,13 +1,22 @@
 package uk.gov.digital.ho.pttg;
 
-import com.google.gson.*;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer;
 import org.junit.Before;
 import org.junit.Test;
 import uk.gov.digital.ho.pttg.api.FinancialStatusRequest;
 import uk.gov.digital.ho.pttg.api.HttpResponse;
 
-import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
@@ -18,18 +27,17 @@ public class SmokeTest {
 
     private String ipApiRootUrl;
     private SimpleHttpClient simpleHttpClient;
-    private Gson gson;
+    private ObjectMapper objectMapper;
 
     @Before
     public void setUp() {
         ipApiRootUrl = getMandatoryEnvVar("IP_API_ROOT_URL");
         simpleHttpClient = new SimpleHttpClient();
-        gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateSerializer())
-                                .create();
+        objectMapper = initialiseObjectMapper(new ObjectMapper());
     }
 
     @Test
-    public void testIpApi() {
+    public void testIpApi() throws JsonProcessingException {
         String body = buildRequest();
         System.out.println(body);
         HttpResponse response = simpleHttpClient.post(ipApiRootUrl + IP_API_PATH, body);
@@ -39,8 +47,8 @@ public class SmokeTest {
         assertThat(response.body()).contains("Resource not found");
     }
 
-    private String buildRequest() {
-        return gson.toJson(FinancialStatusRequest.anyRequest());
+    private String buildRequest() throws JsonProcessingException {
+        return objectMapper.writeValueAsString(FinancialStatusRequest.anyRequest());
     }
 
     private String getMandatoryEnvVar(String envVarName) {
@@ -51,11 +59,18 @@ public class SmokeTest {
         return envVarValue;
     }
 
-    private class LocalDateSerializer implements JsonSerializer<LocalDate> {
+    private static ObjectMapper initialiseObjectMapper(final ObjectMapper objectMapper) {
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.enable(JsonGenerator.Feature.WRITE_NUMBERS_AS_STRINGS);
 
-        @Override
-        public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
-            return new JsonPrimitive(src.toString());
-        }
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-M-d")));
+        javaTimeModule.addDeserializer(YearMonth.class, new YearMonthDeserializer(DateTimeFormatter.ofPattern("yyyy-M")));
+        objectMapper.registerModule(javaTimeModule);
+
+        return objectMapper;
     }
 }
--- src/test/java/uk/gov/digital/ho/pttg/api/Applicant.java
+++ src/test/java/uk/gov/digital/ho/pttg/api/Applicant.java
@@ -1,13 +1,18 @@
 package uk.gov.digital.ho.pttg.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
 class Applicant {
+    @JsonProperty("forename")
     private String forename;
+    @JsonProperty("surname")
     private String surname;
+    @JsonProperty("dateOfBirth")
     private LocalDate dateOfBirth;
+    @JsonProperty("nino")
     private String nino;
 }
