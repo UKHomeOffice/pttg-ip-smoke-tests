67ad70e6162077b5dfbfc7eb1cac71fbc69f2d3e
Owain Roberts <owain.roberts@bjss.com>
2019-08-20 14:50:40 +0100

    EE-6825 Added IpsClient to drive the smoke test call to IPS

--- src/main/java/uk/gov/digital/ho/pttg/testrunner/IpsClient.java
+++ src/main/java/uk/gov/digital/ho/pttg/testrunner/IpsClient.java
@@ -1,4 +1,38 @@
 package uk.gov.digital.ho.pttg.testrunner;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.digital.ho.pttg.testrunner.domain.FinancialStatusRequest;
+
+import java.util.Base64;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 public class IpsClient {
+
+    private final String ipsEndpoint;
+    private final String basicAuth;
+    private RestTemplate restTemplate;
+
+    public IpsClient(String ipsEndpoint, String basicAuth, RestTemplate restTemplate) {
+        this.ipsEndpoint = ipsEndpoint;
+        this.basicAuth = basicAuth;
+        this.restTemplate = restTemplate;
+    }
+
+    public ResponseEntity<Void> sendFinancialStatusRequest(FinancialStatusRequest financialStatusRequest) {
+        return restTemplate.exchange(ipsEndpoint, POST, new HttpEntity<>(financialStatusRequest, getHeaders()), Void.class);
+    }
+
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(APPLICATION_JSON);
+        headers.add(AUTHORIZATION, String.format("Basic %s", Base64.getEncoder().encodeToString(basicAuth.getBytes())));
+        return headers;
+    }
 }
+
--- src/main/java/uk/gov/digital/ho/pttg/testrunner/SmokeTestsService.java
+++ src/main/java/uk/gov/digital/ho/pttg/testrunner/SmokeTestsService.java
@@ -1,11 +1,18 @@
 package uk.gov.digital.ho.pttg.testrunner;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import uk.gov.digital.ho.pttg.api.SmokeTestsResult;
 
 @Component
 public class SmokeTestsService {
     public SmokeTestsResult runSmokeTests() {
+//        try{
+//            ipsClient.sendFinancialStatusRequest(someRequest);
+//            return positive response
+//        } catch (RestClientException e){
+//return failure
+//        }
         return null;
     }
 }
