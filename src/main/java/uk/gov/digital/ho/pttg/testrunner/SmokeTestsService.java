4358b01c793d82d45ef374d261eb2712aba16c2c
Owain Roberts <owain.roberts@bjss.com>
2019-09-26 14:24:29 +0100

    EE-6825 Removing x-component-trace header checking as first step of changing test to expect a NOT_FOUND response from pttg-ip-api

--- src/main/java/uk/gov/digital/ho/pttg/testrunner/SmokeTestsService.java
+++ src/main/java/uk/gov/digital/ho/pttg/testrunner/SmokeTestsService.java
@@ -1,9 +1,7 @@
 package uk.gov.digital.ho.pttg.testrunner;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpStatusCodeException;
 import uk.gov.digital.ho.pttg.api.SmokeTestsResult;
 import uk.gov.digital.ho.pttg.testrunner.domain.Applicant;
 import uk.gov.digital.ho.pttg.testrunner.domain.FinancialStatusRequest;
@@ -11,7 +9,6 @@ import uk.gov.digital.ho.pttg.testrunner.domain.FinancialStatusRequest;
 import java.time.Clock;
 import java.time.LocalDate;
 import java.util.Collections;
-import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -20,27 +17,10 @@ public class SmokeTestsService {
     private static final String TEST_NINO = "QQ123456C";
     private final IpsClient ipsClient;
     private final Clock clock;
-    private final ComponentHeaderChecker componentHeaderChecker;
 
     public SmokeTestsResult runSmokeTests() {
-        try {
-            ipsClient.sendFinancialStatusRequest(someRequest());
-            return failure("Did not expect 200 OK response from IPS");
-        } catch (HttpStatusCodeException e) {
-            return isExpectedComponentTrace(e.getResponseHeaders());
-        }
-    }
-
-    private SmokeTestsResult isExpectedComponentTrace(HttpHeaders headers) {
-        if (headers == null) {
-            return failure("No headers");
-        }
-
-        List<String> componentTraceHeaders = headers.get("x-component-trace");
-        if (componentHeaderChecker.checkAllComponentsPresent(componentTraceHeaders)) {
-            return SmokeTestsResult.SUCCESS;
-        }
-        return failure("Components missing from trace");
+        ipsClient.sendFinancialStatusRequest(someRequest());
+        return failure("Did not expect 200 OK response from IPS");
     }
 
     private FinancialStatusRequest someRequest() {
--- src/test/java/uk/gov/digital/ho/pttg/testrunner/SmokeTestsServiceTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/testrunner/SmokeTestsServiceTest.java
@@ -22,7 +22,6 @@ import java.time.ZoneId;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
@@ -32,7 +31,6 @@ public class SmokeTestsServiceTest {
     @Mock
     private IpsClient mockIpsClient;
     @Mock
-    private ComponentHeaderChecker mockComponentHeaderChecker;
 
     private SmokeTestsService service;
     private ArgumentCaptor<FinancialStatusRequest> requestCaptor;
@@ -44,7 +42,7 @@ public class SmokeTestsServiceTest {
         Instant instant = Instant.from(anyDate.atStartOfDay().atZone(ZoneId.systemDefault()));
         fixedClock = Clock.fixed(instant, ZoneId.systemDefault());
 
-        service = new SmokeTestsService(mockIpsClient, fixedClock, mockComponentHeaderChecker);
+        service = new SmokeTestsService(mockIpsClient, fixedClock);
 
         requestCaptor = ArgumentCaptor.forClass(FinancialStatusRequest.class);
     }
@@ -66,54 +64,6 @@ public class SmokeTestsServiceTest {
         given(mockIpsClient.sendFinancialStatusRequest(any())).willReturn(new ResponseEntity<>(HttpStatus.OK));
 
         assertThat(service.runSmokeTests()).isEqualTo(new SmokeTestsResult(false, "Did not expect 200 OK response from IPS"));
-    }
-
-    @Test
-    public void runSmokeTests_errorNoHeaders_returnFailure() {
-        HttpServerErrorException exceptionWithoutHeaders = exceptionWithHeaders(null);
-        given(mockIpsClient.sendFinancialStatusRequest(any())).willThrow(exceptionWithoutHeaders);
-
-        assertThat(service.runSmokeTests()).isEqualTo(new SmokeTestsResult(false, "No headers"));
-    }
-
-    @Test
-    public void runSmokeTests_noTraceHeader_passNullToChecker() {
-        HttpServerErrorException exceptionWithoutTraceHeader = exceptionWithHeaders(new HttpHeaders());
-        given(mockIpsClient.sendFinancialStatusRequest(any())).willThrow(exceptionWithoutTraceHeader);
-
-        service.runSmokeTests();
-        then(mockComponentHeaderChecker).should().checkAllComponentsPresent(null);
-    }
-
-    @Test
-    public void runSmokeTests_traceHeader_passHeaderToChecker() {
-        String someComponentTrace = "pttg-ip-api,pttg-ip-audit,pttg-ip-hmrc,pttg-ip-audit,HMRC";
-
-        given(mockIpsClient.sendFinancialStatusRequest(any())).willThrow(exceptionWithTrace(someComponentTrace));
-
-        service.runSmokeTests();
-
-        then(mockComponentHeaderChecker).should().checkAllComponentsPresent(singletonList(someComponentTrace));
-    }
-
-    @Test
-    public void runSmokeTests_failureFromChecker_returnFailure() {
-        String anyComponentTrace = "pttg-ip-api,pttg-ip-audit,pttg-ip-hmrc,pttg-ip-audit,HMRC";
-
-        given(mockIpsClient.sendFinancialStatusRequest(any())).willThrow(exceptionWithTrace(anyComponentTrace));
-        given(mockComponentHeaderChecker.checkAllComponentsPresent(anyList())).willReturn(false);
-
-        assertThat(service.runSmokeTests()).isEqualTo(new SmokeTestsResult(false, "Components missing from trace"));
-    }
-
-    @Test
-    public void runSmokeTests_successFromChecker_returnSuccess() {
-        String anyComponentTrace = "pttg-ip-api,pttg-ip-audit,pttg-ip-hmrc,pttg-ip-audit,HMRC";
-
-        given(mockIpsClient.sendFinancialStatusRequest(any())).willThrow(exceptionWithTrace(anyComponentTrace));
-        given(mockComponentHeaderChecker.checkAllComponentsPresent(anyList())).willReturn(true);
-
-        assertThat(service.runSmokeTests()).isEqualTo(SmokeTestsResult.SUCCESS);
     }
 
     private HttpServerErrorException exceptionWithTrace(String componentTrace) {
